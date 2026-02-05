41a8045cab87f4d5465853c8b3aac9ea247b67a2
eltech203 <eltech203@gmail.com>
2026-02-05 23:38:26 +0300

    D

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  console.log("Incoming:", req.method, req.url);
+  next();
+});
 // Test Redis Connection
 // Test Redis Connection at Startup
 
