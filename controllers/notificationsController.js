6b1527491756d7a917a1c59652b9758805a4099b
eltech203 <eltech203@gmail.com>
2026-02-05 23:45:27 +0300

    D

--- controllers/notificationsController.js
+++ controllers/notificationsController.js
@@ -2,18 +2,18 @@ const redisClient = require('../config/redis');
 const { sendNotification } = require("../utils/notify");
 
 // Send Notification
-exports.sendNotification = async (req, res) => {
+exports.sendNotification =  (req, res) => {
     const { message } = req.body;
-    redisClient.publish('notifications', message, (err, reply) => {
+    redisClient.publish('notifications', message, async (err, reply) => {
         if (err) return res.status(500).json({ error: err.message });
-        sendNotification({
+       await sendNotification({
                 user_uid: req.body.uid,
                 user_type: "USER",
                 title: req.body.title,
                 message: req.body.message,
                 type:  "SYSTEM",
                 });
-       return res.json({ message: 'Notification sent', reply });
+        res.json({ message: 'Notification sent', reply });
     });
 };
 
